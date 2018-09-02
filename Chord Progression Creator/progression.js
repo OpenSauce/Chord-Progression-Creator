580018e48ba8176e7ffa50662c9132b3a96c1d77
Lawley <Lawley@LAWLEY-PC>
2018-09-02 14:42:22 +0100

    More javascript

--- Chord Progression Creator/progression.js
+++ Chord Progression Creator/progression.js
@@ -1,2 +1,4 @@
 //Chord Progression Javascript File
 console.log("Select a key and press continue");
+var notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G"];
+var tonality = ["Major", "Minor"];
